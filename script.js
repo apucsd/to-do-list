06670ddef3ab74132387e6a21f1aa9ba2a199219
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-02-11 23:04:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.getElementById('add-btn').addEventListener('click',function(){
+
+
+//     document.getElementById('text-area').addEventListener('keyup',function(e){
+//         const textValue = e.target.value;
+//         console.log(textValue)
+    
+//     });
+//     const h1 = document.getElementById('h1');
+//     h1.innerText = textValue;
+// })
